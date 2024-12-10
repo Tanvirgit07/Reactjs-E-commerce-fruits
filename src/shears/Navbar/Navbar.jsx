23ca01565bdca03260c309c20bb59409de27a9f7
Tanvir Ahmmed <tanvirgithub2024@gmail.com>
2024-12-11 00:13:24 +0600

    clear navbar unuse code

--- src/shears/Navbar/Navbar.jsx
+++ src/shears/Navbar/Navbar.jsx
@@ -1,22 +1,32 @@
 import { Link } from "react-router-dom";
 
 const Navbar = () => {
+
   return (
     <div className="flex justify-between items-center bg-[#FFFFFF] px-10 py-5 w-full">
       <div>
         <img src="https://i.ibb.co.com/j80x2Gr/logo.png" alt="shop_logo" />
       </div>
 
+
       <div className="flex items-center divide-x gap-4">
         <div className="flex gap-10 text-sm font-bold">
           <Link to='/'>HOME</Link>
+
           <Link to='/brand'>BRANDS</Link>
+
           <Link>CONTACT US</Link>
+
           <Link>SPECIALS</Link>
+
           <Link>BLOGS</Link>
+
         </div>
+
         <div className="flex gap-2 ml-3">
+
           <div className="ml-4">
+            
             <img src="https://i.ibb.co.com/Z25JXmd/phn.png" alt="phn image" />
           </div>
           <div>
